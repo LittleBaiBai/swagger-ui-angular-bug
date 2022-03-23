776e0972f6fe530084e25e247691fc04651fadbe
Yuxin Bai <bellab@vmware.com>
2022-03-23 16:49:20 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as SwaggerUI from 'swagger-ui';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
 }
+
+const ui = SwaggerUI({
+  dom_id: '#swagger-ui',
+  spec: null
+});
